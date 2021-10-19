435c3e90c5814c9251b26e6a5edda73af1ec4167
Xavier Mathieu <xavier2003@hotmail.fr>
2021-10-19 08:17:16 -0400

    Initial commit

---
+++ src/cegepst/engine/GameWindow.java
@@ -0,0 +1,86 @@
+package cegepst.engine;
+
+import cegepst.Ball;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class GameWindow extends JFrame {
+
+    private static final int SLEEP = 25;
+    private Ball ball;
+    private boolean playing = true;
+    private JPanel panel;
+    private BufferedImage bufferedImage;
+    private Graphics2D buffer;
+    private long before;
+    private int score = 0;
+
+    public GameWindow() {
+        setSize(800, 600);
+        setLocationRelativeTo(null); // Center frame on screen
+        setResizable(false);
+        setTitle("Bouncing Balls");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setState(JFrame.NORMAL);
+        // setUndecorated(true); // Supprimer la barre de l'application
+
+        panel = new JPanel();
+        panel.setBackground(Color.blue);
+        panel.setFocusable(true);
+        panel.setDoubleBuffered(true);
+        add(panel); // Ajouter le panneau dans le JFrame
+
+        ball = new Ball();
+    }
+
+    public void start() {
+        setVisible(true);
+        before = System.currentTimeMillis();
+        while (playing) {
+            bufferedImage = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
+            buffer = bufferedImage.createGraphics();
+            RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            buffer.setRenderingHints(rh);
+
+            update();
+            drawOnBuffer();
+            drawBufferOnScreen();
+
+            long sleep = SLEEP - (System.currentTimeMillis() - before);
+            if (sleep < 0) {
+                sleep = 4;
+            }
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            before = System.currentTimeMillis();
+        }
+    }
+
+    public void update() {
+        ball.update();
+        if (ball.hasTouchBound()) {
+            score += 10;
+        }
+    }
+
+    public void drawOnBuffer() {
+        buffer.setPaint(Color.red);
+        buffer.fillOval(ball.getX(), ball.getY(), ball.getRadius() * 2, ball.getRadius() * 2);
+
+        buffer.setPaint(Color.WHITE);
+        buffer.drawString("Score: " + score, 10, 20);
+    }
+
+    public void drawBufferOnScreen() {
+        Graphics2D graphics = (Graphics2D) panel.getGraphics();
+        graphics.drawImage(bufferedImage, 0, 0, panel);
+        Toolkit.getDefaultToolkit().sync();
+        graphics.dispose();
+    }
+}
