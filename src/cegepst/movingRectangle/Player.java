0b5189588694747fe47f60bfa17b3478a7a822a5
Xavier Mathieu <xavier2003@hotmail.fr>
2021-10-26 09:32:59 -0400

    Implémentation de speed pour l'entité player

--- src/cegepst/movingRectangle/Player.java
+++ src/cegepst/movingRectangle/Player.java
@@ -2,7 +2,6 @@ package cegepst.movingRectangle;
 
 import cegepst.engine.Buffer;
 import cegepst.entities.ControllableEntity;
-import cegepst.entities.MovableEntity;
 
 import java.awt.*;
 
@@ -11,18 +10,19 @@ public class Player extends ControllableEntity {
     public Player(int x, int y) {
         teleport(x, y);
         setDimension(30, 60);
+        setSpeed(4);
     }
 
     @Override
     public void update(InputHandler inputHandler) {
         if (inputHandler.isDownPressed()) {
-            y += 4;
+            moveDown();
         } else if (inputHandler.isUpPressed()) {
-            y -= 4;
+            moveUp();
         } else if (inputHandler.isLeftPressed()) {
-            x -= 4;
+            moveLeft();
         } else if (inputHandler.isRightPressed()) {
-            x += 4;
+            moveRight();
         }
     }
 
