a902e405f262980d979d2051a9f61271c25b2331
Xavier Mathieu <xavier2003@hotmail.fr>
2021-10-29 11:47:49 -0400

    Fin du jeu

--- src/cegepst/movingRectangle/MovingRectangleGame.java
+++ src/cegepst/movingRectangle/MovingRectangleGame.java
@@ -4,20 +4,33 @@ import cegepst.engine.Buffer;
 import cegepst.engine.Game;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class MovingRectangleGame extends Game {
 
     private GamePad controllerOne;
+    private GamePad controllerTwo;
     private Player playerOne;
+    private Player playerTwo;
     private ArrayList<Footprint> footprints;
 
     @Override
     public void initialize() {
         controllerOne = new GamePad();
+        controllerTwo = new GamePad();
+        controllerTwo.setUpKey(KeyEvent.VK_W);
+        controllerTwo.setDownKey(KeyEvent.VK_S);
+        controllerTwo.setRightKey(KeyEvent.VK_D);
+        controllerTwo.setLeftKey(KeyEvent.VK_A);
+
         playerOne = new Player(controllerOne, Color.PINK);
+        playerTwo = new Player(controllerTwo, Color.GREEN);
+
         footprints = new ArrayList<>();
         addKeyListener(controllerOne);
+        addKeyListener(controllerTwo);
+
     }
 
     @Override
@@ -26,8 +39,12 @@ public class MovingRectangleGame extends Game {
             stop();
         }
         playerOne.update();
+        playerTwo.update();
         if (controllerOne.isMoving()) {
             footprints.add(playerOne.layFootprint());
+        }
+        if (controllerTwo.isMoving()) {
+            footprints.add(playerTwo.layFootprint());
         }
     }
 
@@ -37,6 +54,7 @@ public class MovingRectangleGame extends Game {
             footprint.draw(buffer);
         }
         playerOne.draw(buffer);
+        playerTwo.draw(buffer);
     }
 
     @Override
