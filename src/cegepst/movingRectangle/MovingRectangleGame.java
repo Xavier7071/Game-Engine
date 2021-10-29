b28ff9f3bf8828e15e78fdcf15f94fb9d2016932
Xavier Mathieu <xavier2003@hotmail.fr>
2021-10-29 11:35:44 -0400

    Intégration de footprints dans le jeu

--- src/cegepst/movingRectangle/MovingRectangleGame.java
+++ src/cegepst/movingRectangle/MovingRectangleGame.java
@@ -3,16 +3,21 @@ package cegepst.movingRectangle;
 import cegepst.engine.Buffer;
 import cegepst.engine.Game;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class MovingRectangleGame extends Game {
 
     private GamePad controllerOne;
     private Player playerOne;
+    private ArrayList<Footprint> footprints;
 
     @Override
     public void initialize() {
         controllerOne = new GamePad();
+        playerOne = new Player(controllerOne, Color.PINK);
+        footprints = new ArrayList<>();
         addKeyListener(controllerOne);
-        playerOne = new Player(controllerOne);
     }
 
     @Override
@@ -21,10 +26,16 @@ public class MovingRectangleGame extends Game {
             stop();
         }
         playerOne.update();
+        if (controllerOne.isMoving()) {
+            footprints.add(playerOne.layFootprint());
+        }
     }
 
     @Override
     public void draw(Buffer buffer) {
+        for (Footprint footprint : footprints) {
+            footprint.draw(buffer);
+        }
         playerOne.draw(buffer);
     }
 
